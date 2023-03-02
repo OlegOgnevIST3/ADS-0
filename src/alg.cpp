69d4aed27359abd6189dfbe54d651430ba0b433b
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-03-02 20:08:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,11 +2,11 @@
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
     while (a != 0 && b != 0) {
-        if (a > b) {
-            a %= b;
+    if (a > b) {
+        a %= b;
         }
-        else {
-            b %= a;
+    else {
+        b %= a;
         }
     }
     return a+b;
