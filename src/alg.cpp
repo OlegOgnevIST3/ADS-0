16a7cb675f7414f7a419c3b77df70459b2f96e84
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-03-02 18:27:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,13 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
-  if (a % b == 0) {
-    return b;
-  }
-  if (b % a == 0) {
-    return a;
-  }
-  if (a > b) {
-    return gcd(a%b, b); 
-  }
-  return gcd(a, b%a);
+    while (a > 0 && b > 0) {
+        if (a > b) {
+            a -= b;
+        }
+        else {
+            b -= a;
+        }
+    }
+    return a+b;
 }
