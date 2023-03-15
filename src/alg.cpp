06e2b1db723d3d60156d7156e5d4210e2fedbc87
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-03-15 20:12:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
+
 int gcd(int a, int b) {
   while (a != 0 && b != 0) {
-      if (a > b)
-        a = a % b;
-      else
-        b = b % a;
+  if (a > b)
+    a = a % b;
+  else
+    b = b % a;
   }
-  return a + b;    
+  return a+b;    
 }
